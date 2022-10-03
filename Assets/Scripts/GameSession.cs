a30a44495a84229bdfe6385d5b433199992ecaab
seniordaniel1 <danielbolanos0803@gmail.com>
2022-10-03 10:29:18 -0400

    Add Tile Project

---
+++ Assets/Scripts/GameSession.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class GameSession : MonoBehaviour
+{
+    [SerializeField] int playerLives = 3;
+    [SerializeField] TextMeshProUGUI livesText;
+    void Awake()
+    {
+        int numGameSessions = FindObjectsOfType<GameSession>().Length;
+        if (numGameSessions > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+    void Start()
+    {
+        livesText.text = playerLives.ToString();
+    }
+
+    public void ProcessPlayerDeath()
+    {
+        if(playerLives > 1)
+        {
+            TakeLife();
+        }
+        else
+        {
+            ResetGameSession();
+        }
+    }
+    void ResetGameSession()
+    {
+        SceneManager.LoadScene(0);
+        Destroy(gameObject);
+    }
+    void TakeLife()
+    {
+        playerLives--;
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+                livesText.text = playerLives.ToString();
+
+    }
+}
